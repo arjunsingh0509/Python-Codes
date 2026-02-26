9cc876f8114f55aa198ea25453e4db4118081977
arjunsingh0509 <arjunsingh.engineer27@gmail.com>
2026-02-27 00:03:18 +0530

    Add files via upload

---
+++ Matchcase.py
@@ -0,0 +1,12 @@
+x=22
+match x:
+    case 1:
+        print("1")
+    case 2:
+        print("2")
+    case 3:
+        print("3")
+    case _:
+        print(x)
+        #break is not used as c c++
+        
